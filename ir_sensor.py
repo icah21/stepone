404e33531e33b7f66c95d663d3233dcfb065d919
Jamaica Quizzagan <jamaicaquizzagan21@gmail.com>
2025-05-09 12:23:50 +0800

    testquatro

--- ir_sensor.py
+++ ir_sensor.py
@@ -4,8 +4,7 @@ class IRSensor:
     def __init__(self, pin):
         self.pin = pin
         GPIO.setmode(GPIO.BCM)
-        GPIO.setwarnings(False)
         GPIO.setup(self.pin, GPIO.IN)
 
     def is_object_detected(self):
-        return GPIO.input(self.pin) == 0  # Change to 1 if using normally-open sensor
+        return GPIO.input(self.pin) == 0  # adjust to 1 if needed
